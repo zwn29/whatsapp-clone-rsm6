4c06949b17fd36c30fd566b02ee55c3ce08966af
zenf <78023279+zwn28@users.noreply.github.com>
2022-11-02 20:33:07 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBfGwmAF6g1yJCiB9nE9zwra4JWh3qa-YU',
-  authDomain: 'whatsapp-clone-c42a4.firebaseapp.com',
-  projectId: 'whatsapp-clone-c42a4',
-  storageBucket: 'whatsapp-clone-c42a4.appspot.com',
-  messagingSenderId: '990911877508',
-  appId: '1:990911877508:web:69003a78607edd586a0dd1',
+ /// enter your confg here
 }
 
 const app = !firebase.apps.length
